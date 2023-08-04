9fadc950b638964dcb888d83142eb4f43b68037e
Arak829 <enocjunior13@gmail.com>
2023-08-04 13:38:01 -0500

    firts commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+const snap7 = require('node-snap7');
+
+app.use(cors());
+
+
+const s7client = new snap7.S7Client();
+s7client.ConnectTo('192.168.1.10', 0, 2, function(err) {
+    if(err)
+        return console.log(' >> Connection failed. Code #' + err + ' - ' + s7client.ErrorText(err));
+
+    // Read the first byte from PLC process outputs...
+    s7client.ReadArea(s7client.S7AreaDB, 11, 0, 6, s7client.S7WLByte,function (err, buf) {
+        if (err) {
+            console.log(err);
+        }
+        console.log(buf);
+        })
+}); 
