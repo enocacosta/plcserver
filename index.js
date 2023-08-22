0fa913fd4be135ac0896a640e1e735d73c57ed0e
sebasguti49@gmail.com <sebasguti49@gmail.com>
2023-08-22 14:24:47 -0500

    leer un solo bit

--- index.js
+++ index.js
@@ -10,18 +10,30 @@ s7client.SetParam(1,51000);//try first param in 1 in order to see if that modify
 s7client.ConnectTo('192.168.1.10', 0, 2, function(err) {
     if(err)
         return console.log(' >> Connection failed. Code #' + err + ' - ' + s7client.ErrorText(err));
-
-    // Read the first byte from PLC process outputs...
-    setInterval(function(){
-        s7client.ReadArea(s7client.S7AreaDB, 14 , 0, 15, s7client.S7WLByte,function (err, buf) {
-            if (err) {
-                console.log(err);
-            }
-            console.log(buf);
-            })   
-    },5000);
    
 }); 
+
+// Read the first byte from PLC process outputs...
+setInterval(function(){
+    s7client.ReadArea(s7client.S7AreaDB, 14 , 112, 1, s7client.S7WLBit,function (err, buf) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Lectura 1");
+        console.log(buf);
+        })   
+},5000); 
+
+// Read the first byte from PLC process outputs... leer bloque leer entre nose que
+// setInterval(function(){
+//     s7client.ReadArea(s7client.S7AreaDB, 13 , 16, 1, s7client.S7WLBit,function (err, buf1) {
+//         if (err) {
+//             console.log(err);
+//         }
+//         console.log("Lectura 2");
+//         console.log(buf1);
+//         })   
+// },5000); 
 
 s7client.Disconnect();
 
