f4125c13e20ba10ad6d18522727871828ff7e90b
sebastian cruz <sebasguti49@gmail.com>
2023-08-27 16:21:31 -0500

    s7client connect

--- server/config/conexionPLC.js
+++ server/config/conexionPLC.js
@@ -1,11 +1,13 @@
 const { S7Client } = require('node-snap7');
 const s7client = new S7Client();
 
+
+
 const connectToPLC = () => {
     return new Promise((resolve, reject) => {
-        s7client.ConnectTo('192.168.1.10', 0, 2, (err) => {
+        s7client.ConnectTo('192.168.0.1', 0, 1, (err) => {
             if (err) {
-                reject(new Error(`Error promesa conexion plc: ${err}`));
+                reject(new Error(`Error promesa conexion plc: ${err} - ${s7client.ErrorText(err)}` ));
             } else {
                 resolve('Conexión exitosa');
             }
