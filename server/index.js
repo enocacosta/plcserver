4444fc56a0accff929f95ad1f7cbbdf9936e905c
sebasguti49@gmail.com <sebasguti49@gmail.com>
2023-08-28 16:28:22 -0500

    commit

--- server/index.js
+++ server/index.js
@@ -1,48 +1,41 @@
 const express = require('express')
 const cors = require('cors');
 const app = express();
-const connectionPLC = require('./config/conexionPLC')
-const getinforPLC = require('./helpers/getinfoPLC')
-const setParam = require('./helpers/setParam')
 
-setParam();
-connectionPLC()
-const result = getinforPLC();
-getinforPLC()
-console.log(result);
-app.use(cors());
 
-// const s7client = new snap7.S7Client();
+const {S7Client} = require('s7client');
 
-// s7client.ConnectTo('192.168.1.10', 0, 2, function(err) {
-//     if(err) {
-//         return console.log(' >> Connection failed. Code #' + err + ' - ' + s7client.ErrorText(err));
-//     }else{
-//         console.log('Connection succesful');
-//     }
-// }); 
+// PLC Connection Settings
+const plcSettings = {
+  name: "LocalPLC",
+  host: '192.168.1.10',
+  port: 102,
+  rack: 0,
+  slot: 2
+};
 
-// Read the first byte from PLC process outputs...
-// setInterval(function(){
-//     s7client.ReadArea(s7client.S7AreaDB, 14 , 0, 14, s7client.S7WLBit,function (err, buf) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         console.log("Lectura 1");
-//         console.log(buf);
-//         })   
-// },5000); 
+// DBA to read
+let dbNr = 14;
+let dbVars = [{
+    ident: 'Int',
+    type: 'INT',
+    start: 4,
+}
+]
 
-// // Read the first byte from PLC process outputs... leer un solo byte
-// setInterval(function(){
-//     s7client.ReadArea(s7client.S7AreaDB, 13 , 16, 1, s7client.S7WLBit,function (err, buf1) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         console.log("Lectura 2");
-//         console.log(buf1);
-//         })   
-// },5000); 
+let client = new S7Client(plcSettings);
+client.on('error', console.error);
+
+(async function() {
+    await client.setParam(1,5000);
+  await client.connect();
+
+  // Read DB
+  const res = await client.readDB(dbNr, dbVars);
+  console.log(res);
+
+  client.disconnect();
+})();
 
 
 
