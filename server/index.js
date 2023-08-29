885914bbb7f8c5417c014fc4a1f3e8fb394045ef
sebasguti49@gmail.com <sebasguti49@gmail.com>
2023-08-28 20:24:13 -0500

    getting response ready to save in DB

--- server/index.js
+++ server/index.js
@@ -1,48 +1,49 @@
 const express = require('express')
 const cors = require('cors');
+var snap7 = require('node-snap7');
 const app = express();
-const connectionPLC = require('./config/conexionPLC')
-const getinforPLC = require('./helpers/getinfoPLC')
-const setParam = require('./helpers/setParam')
+const conexionDB = require('./config/conexionDB');
+const Modelo = require('./model/modeloDB');
 
-setParam();
-connectionPLC()
-const result = getinforPLC();
-getinforPLC()
-console.log(result);
+conexionDB();
 app.use(cors());
 
-// const s7client = new snap7.S7Client();
+const s7client = new snap7.S7Client();
 
-// s7client.ConnectTo('192.168.1.10', 0, 2, function(err) {
-//     if(err) {
-//         return console.log(' >> Connection failed. Code #' + err + ' - ' + s7client.ErrorText(err));
-//     }else{
-//         console.log('Connection succesful');
-//     }
-// }); 
+s7client.ConnectTo('192.168.1.10', 0, 2, function(err) {
+    if(err) {
+        return console.log(' >> Connection failed. Code #' + err + ' - ' + s7client.ErrorText(err));
+    }else{
+        console.log('Connection succesful');
+    }
+}); 
 
-// Read the first byte from PLC process outputs...
-// setInterval(function(){
-//     s7client.ReadArea(s7client.S7AreaDB, 14 , 0, 14, s7client.S7WLBit,function (err, buf) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         console.log("Lectura 1");
-//         console.log(buf);
-//         })   
-// },5000); 
 
-// // Read the first byte from PLC process outputs... leer un solo byte
-// setInterval(function(){
-//     s7client.ReadArea(s7client.S7AreaDB, 13 , 16, 1, s7client.S7WLBit,function (err, buf1) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         console.log("Lectura 2");
-//         console.log(buf1);
-//         })   
-// },5000); 
+//Read the first byte from PLC process outputs...
+setInterval(function(){
+    s7client.ReadArea(s7client.S7AreaDB, 14 , 0, 14, s7client.S7WLByte,function (err, buf) {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Lectura 1");
+        console.log(buf);
+        let array='';
+        for(let i=0; i<14;i++){
+          buffer=buf[i].toString(16);
+          console.log(buffer);
+          array += buffer + ' ';
+        }
+        array.trim();
+        console.log(array);
+
+        })   
+},5000); 
+
+
+
+
+
+
 
 
 
