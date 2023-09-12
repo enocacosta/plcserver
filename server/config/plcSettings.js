10f75c741283410640ee7d02a40854409e6fd3e8
Arak829 <enocjunior13@gmail.com>
2023-09-12 13:02:02 -0500

    bascula empresa

--- server/config/plcSettings.js
+++ server/config/plcSettings.js
@@ -2,11 +2,11 @@ const {S7Client} = require('s7client');
 
 // PLC Connection Settings
 const plcSettings = {
-    name: "LocalPLC",
-    host: '192.168.1.10',
+    name: "Llenadora_PLC",
+    host: '192.168.1.100',
     port: 102,
     rack: 0,
-    slot: 2
+    slot: 1
   };
 
 let client = new S7Client(plcSettings);
