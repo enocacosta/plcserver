c586dc7c3af29466fcbc91aa0e43475bebe3718b
Arak829 <enocjunior13@gmail.com>
2023-10-24 10:26:49 -0500

    2393

---
+++ server/model/modeloRequest.js
@@ -0,0 +1,19 @@
+const multiVars = 
+[
+    {
+        "Area": s7client.S7AreaDB,
+        "WordLen": s7client.S7WLByte,
+        "DBNumber": 14,
+        "Start": 0,
+        "Amount": 14
+    },
+    {
+        "Area": s7client.S7AreaDB,
+        "WordLen": s7client.S7WLByte,
+        "DBNumber":11,
+        "Start": 16,
+        "Amount": 1
+      },
+]
+
+module.exports = multiVars
